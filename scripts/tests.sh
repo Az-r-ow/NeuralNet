60acb47de4d6fc41feaeb36693473b6fc84f9e5e
Az-r-ow <antoine.azar123@gmail.com>
2023-08-19 15:44:07 +0200

    update: Added shebang to script files + dynamic execution of tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
+# Accessing the tests folder
 cd build/tests
-./test-functions
-./test-network
+
+# looping of the tests 
+for file in ./*; do 
+  if [[ -f "$file" && -x "$file" ]]; then
+    echo "Running : $file"
+    # executing the test files
+    $file
+  fi 
+done 
+
+# Navigating back to the main folder
 cd ../..
