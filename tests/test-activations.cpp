5702b34ad9f728739f6f9994257fc2406998f126
Az-r-ow <antoine.azar123@gmail.com>
2023-08-19 15:01:05 +0200

    refactor: Made the tests build more dynamic

---
+++ tests/test-activations.cpp
@@ -0,0 +1,4 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <activations/Relu.hpp>
+#include <activations/Sigmoid.hpp>
--- tests/test-functions.cpp
+++ tests/test-functions.cpp
@@ -1,34 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <Functions.hpp>
+#include <utils/Functions.hpp>
 
 using namespace NeuralNet;
 using namespace Catch::Matchers;
 
 const double ERR_MARGIN = 0.001;
-
-TEST_CASE("Relu of negative numbers always returns 0", "[function]")
-{
-  REQUIRE(relu(-1) == 0);
-  REQUIRE(relu(-898.99) == 0);
-  REQUIRE(relu(-0) == 0);
-}
-
-TEST_CASE("Relu of positive numbers always returns the number itself", "[function]")
-{
-  REQUIRE(relu(0) == 0);
-  REQUIRE(relu(200) == 200);
-  REQUIRE(relu(99999999999999) == 99999999999999);
-}
-
-TEST_CASE("Sigmoid returns the right answers", "[function]")
-{
-  REQUIRE(sigmoid(0) == 0.5);
-  REQUIRE_THAT(sigmoid(1), WithinAbs(0.731, ERR_MARGIN));
-  REQUIRE_THAT(sigmoid(10), WithinAbs(1, ERR_MARGIN));
-  REQUIRE_THAT(sigmoid(-1), WithinAbs(0.268, ERR_MARGIN));
-  REQUIRE_THAT(sigmoid(-10), WithinAbs(0, ERR_MARGIN));
-}
 
 TEST_CASE("Sqr function returns the right square", "[function]")
 {
