1db0dfc896ab1df8973705c6b7b5273e203a0f16
Az-r-ow <antoine.azar123@gmail.com>
2023-08-28 14:52:19 +0200

    feat(examples): script to load the mnist dataset into a numpy array

--- examples/train-predict-MNIST/main.py
+++ examples/train-predict-MNIST/main.py
@@ -1,5 +1,13 @@
-import sys, os 
+import sys, os , requests
+import numpy as np
+from utils import *
 
+"""
+  In the first few lines we add the build folder to the sys.paths 
+  to be able to import the NeuralNet module from the generated .so file
+"""
+
+# Getting the path to the .so file 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 so_dir = os.path.join(script_dir, "..", "..", "build")
@@ -8,6 +16,20 @@ so_dir = os.path.join(script_dir, "..", "..", "build")
 sys.path.append(so_dir)
 
 import NeuralNetPy as NNP
+
+mnist_dataset_file = "./dataset/mnist.npz"
+mnist_dataset_url = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz"
+
+# If file doesn't exists create and download the data
+if not os.path.exists(mnist_dataset_file):
+  print(f"{mnist_dataset_file} not found")
+  print("Downloading the dataset")
+  response = requests.get(mnist_dataset_url)
+  with open(mnist_dataset_file, 'wb') as f:
+    f.write(response.content)
+
+# Otherwise load data from file
+(x_train, y_train), (x_test, y_test) = load_data(mnist_dataset_file)
 
 network = NNP.Network()
 
---
+++ examples/train-predict-MNIST/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def load_data(path):
+    with np.load(path) as f:
+        x_train, y_train = f['x_train'], f['y_train']
+        x_test, y_test = f['x_test'], f['y_test']
+        return (x_train, y_train), (x_test, y_test)
+
