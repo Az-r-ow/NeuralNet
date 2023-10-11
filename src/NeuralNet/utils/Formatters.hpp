96cb79343d25155f3c3f66b73f00e6e17aef559d
Az-r-ow <antoine.azar123@gmail.com>
2023-10-11 18:01:38 +0200

    nfc(major-refactor): format labels function returns labels matrix

--- src/NeuralNet/utils/Formatters.hpp
+++ src/NeuralNet/utils/Formatters.hpp
@@ -2,6 +2,7 @@
 
 #include <Eigen/Core>
 #include "Typedefs.hpp"
+#include "Functions.hpp"
 
 namespace NeuralNet
 {
@@ -34,6 +35,51 @@ namespace NeuralNet
     assert(y.size() == size);
     return Eigen::MatrixXd::Map(&y[0], size, 1);
   };
+
+  /**
+   * @brief This function transform the labels in 2d or 1d vectors into Matrices
+   *
+   * The resulting matrices will be used to evaluate the model's performance and do the necessary adjustments on its parameters
+   *
+   * @param labels 1d std::vector or 2d std::vector (based on the task)
+   * @param shape std::tuple that defines the shape of the resulting matrix
+   *
+   * @return The resulting matrix from the passed labels
+   */
+  template <typename T>
+  static Eigen::MatrixXd formatLabels(std::vector<T> labels, std::tuple<int, int> shape)
+  {
+    int rows = std::get<0>(shape);
+    int cols = std::get<1>(shape);
+
+    // When propagating forward, the number of rows will be the number of inputs
+    assert(labels.size() == rows && "The number of labels don't match the number of inputs");
+
+    Eigen::MatrixXd mLabels(rows, cols);
+
+    if (std::is_same<T, std::vector<double>>)
+    {
+      std::vector<double> flattenedVector = flatten2DVector(labels, rows, cols);
+      mLabels = Eigen::Map<Eigen::MatrixXd>(flattenedVector.data(), rows, cols);
+    }
+    else if (std::is_same<T, double>)
+    {
+      mLabels = Eigen::MatrixXd::Zero(rows, cols);
+
+      /**
+       * Setting the cols indexes to 1
+       * Notably used in classification tasks
+       */
+      for (int i = 0; i < rows; i++)
+      {
+        int colIndex = labels[i];
+        assert(colIndex < cols);
+        mLabels(i, colIndex) = 1;
+      }
+    }
+
+    return mLabels;
+  }
 
   /**
    * @brief Converts layer's outputs from Eigen::Eigen::MatrixXd to an std::vector
