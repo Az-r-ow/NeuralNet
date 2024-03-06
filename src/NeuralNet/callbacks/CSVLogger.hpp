f0b4359b2030110fe58ca22a0c846eeb708b4be9
Az-r-ow <antoine.azar123@gmail.com>
2024-03-06 19:19:21 +0100

    fix: included stdexcep header

--- src/NeuralNet/callbacks/CSVLogger.hpp
+++ src/NeuralNet/callbacks/CSVLogger.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <fstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
--- src/NeuralNet/callbacks/Callback.hpp
+++ src/NeuralNet/callbacks/Callback.hpp
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <memory>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <utility>
