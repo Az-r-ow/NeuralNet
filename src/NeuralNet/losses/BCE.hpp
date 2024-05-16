1af5dea9392022508ca3c0451a42911aef702947
Az-r-ow <antoine.azar123@gmail.com>
2024-05-16 21:12:32 +0200

    test: BCE test case with softmax

--- src/NeuralNet/losses/BCE.hpp
+++ src/NeuralNet/losses/BCE.hpp
@@ -9,14 +9,21 @@ namespace NeuralNet {
 class BCE : public Loss {
  public:
   static double cmpLoss(const Eigen::MatrixXd &o, const Eigen::MatrixXd &y) {
-    Eigen::MatrixXd loss = -(y.array() * o.array().log() +
-                             (1.0 - y.array()) * (1.0 - o.array()).log());
+    double threshold = 1.0e-5;
+    Eigen::MatrixXd oTrim = trim(o, threshold);
+    Eigen::MatrixXd yTrim = trim(y, threshold);
+
+    Eigen::MatrixXd loss =
+        -(yTrim.array() * oTrim.array().log() +
+          (1.0 - yTrim.array()) * (1.0 - oTrim.array()).log());
+
     return loss.sum();
   }
 
   static Eigen::MatrixXd cmpLossGrad(const Eigen::MatrixXd &yHat,
                                      const Eigen::MatrixXd &y) {
-    return (yHat.array() - y.array()) / (yHat.array() * (1.0 - y.array()));
+    return (yHat.array() - y.array()) /
+           ((yHat.array() * (1.0 - yHat.array())) + 1e-9);
   }
 };
 
