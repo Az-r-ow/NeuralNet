60fc2550fad9fcf698f16915a8c9264072ed7de1
Az-r-ow <Az-r-ow@users.noreply.github.com>
2024-07-06 13:35:46 +0000

    deploy: a2f83906355892431597f1b5ab237ed7f423e10f

--- cpp-docs/search/functions_6.js
+++ cpp-docs/search/functions_6.js
@@ -7,5 +7,6 @@ var searchData=
   ['getnumneurons_4',['getNumNeurons',['../classNeuralNet_1_1Layer.html#afa59ddacc5796712e5061ed055833fde',1,'NeuralNet::Layer']]],
   ['getoutputlayer_5',['getOutputLayer',['../classNeuralNet_1_1Network.html#ae4f8858beaf24d4b2e038e2db67db625',1,'NeuralNet::Network']]],
   ['getoutputs_6',['getOutputs',['../classNeuralNet_1_1Dense.html#ab4c9fc65d181dc0a559ae861fcfe3cbe',1,'NeuralNet::Dense::getOutputs()'],['../classNeuralNet_1_1Layer.html#a1b86e971a93bed55690ecf1840207c34',1,'NeuralNet::Layer::getOutputs()']]],
-  ['getweights_7',['getWeights',['../classNeuralNet_1_1Dense.html#a8ab39a273bab4ac8d9e43c04750cfe78',1,'NeuralNet::Dense']]]
+  ['getslug_7',['getSlug',['../classNeuralNet_1_1Dense.html#a6b08c9574a995b8859e0c47381adde39',1,'NeuralNet::Dense::getSlug()'],['../classNeuralNet_1_1Dropout.html#a7de97c764db64de66e69f7f7878e1427',1,'NeuralNet::Dropout::getSlug()'],['../classNeuralNet_1_1Flatten.html#afb284d8ca538531184840dc67e44c0d2',1,'NeuralNet::Flatten::getSlug()'],['../classNeuralNet_1_1Layer.html#a204b844152e118a8772b0b46696555f8',1,'NeuralNet::Layer::getSlug()'],['../classNeuralNet_1_1Network.html#a3ec28923e1d823fb1b8269c859d0647c',1,'NeuralNet::Network::getSlug()']]],
+  ['getweights_8',['getWeights',['../classNeuralNet_1_1Dense.html#a8ab39a273bab4ac8d9e43c04750cfe78',1,'NeuralNet::Dense']]]
 ];
